9d2e2b19e9cf3187f39e40bf99c2c5be06615ea9
Bahtiyar <bahtiyar_t9581@list.ru>
2023-02-11 19:22:50 +0300

    Добавил код для игры "крестики нолики"

---
+++ krestiki.py
@@ -0,0 +1,77 @@
+def field_check():
+    print()
+    print("    | 0 | 1 | 2 | ")
+    print("  --------------- ")
+    for i, row in enumerate(field):
+        row_str = f"  {i} | {' | '.join(row)} | "
+        print(row_str)
+        print("  --------------- ")
+    print()
+
+
+def move():
+    while True:
+        cords = input("         Ваш ход, введите координаты через пробел: ").split()
+
+        if len(cords) != 2:
+            print(" Введите 2 координаты! ")
+            continue
+
+        x, y = cords
+
+        if not (x.isdigit()) or not (y.isdigit()):
+            print(" Введите числа! ")
+            continue
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print(" Координаты вне диапазона! ")
+            continue
+
+        if field[x][y] != " ":
+            print(" Клетка занята! ")
+            continue
+
+        return x, y
+
+
+def check_win():
+    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for cord in win_cord:
+        symbols = []
+        for c in cord:
+            symbols.append(field[c[0]][c[1]])
+        if symbols == ["X", "X", "X"]:
+            print("Выиграл X!!!")
+            return True
+        if symbols == ["0", "0", "0"]:
+            print("Выиграл 0!!!")
+            return True
+    return False
+
+field = [[" "] * 3 for i in range(3)]
+count = 0
+while True:
+    count += 1
+    field_check()
+    if count % 2 == 1:
+        print(" Ходит крестик!")
+    else:
+        print(" Ходит нолик!")
+
+    x, y = move()
+
+    if count % 2 == 1:
+        field[x][y] = "X"
+    else:
+        field[x][y] = "0"
+
+    if check_win():
+        break
+
+    if count == 9:
+        print(" Ничья!")
+        break
